8cc16910dbdc70ed6794dd663172c2325ebf36bb
msdubova <femme777@gmail.com>
2019-06-26 19:50:28 +0200

    Добавила обнуление координат попапа при повторном открытии попапа

--- js/dialog.js
+++ js/dialog.js
@@ -1,50 +1,68 @@
 'use strict';
 
-var dialogHandler = document.querySelector('.upload');
-var setup = document.querySelector('.setup');
 
-dialogHandler.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
+var popup = document.querySelector('.setup');
+var dialogHandler = popup.querySelector('.upload');
+var popupOpen = document.querySelector('.setup-open');
+var popupClose = popup.querySelector('.setup-close');
+var defaultX = popup.style.top;
+var defaultY = popup.style.left;
 
-  var dragged = false;
+var defaultPopupCoordinates = function () {
+  popup.style.top = defaultX;
+  popup.style.left = defaultY;
+};
 
-  var onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-    dragged = true;
+var onDialogHandlerClick = function () {
+  popup.style.top = defaultX;
+  popup.style.left = defaultY;
 
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
     };
 
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
+    var dragged = false;
 
-    setup.style.top = (setup.offsetTop - shift.y) + 'px';
-    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
-  };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
 
-  var onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
-
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-    if (dragged) {
-      var onClickPreventDefault = function (evtDrgd) {
-        evtDrgd.preventDefault();
-        dialogHandler.removeEventListener('click', onClickPreventDefault);
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
       };
-      dialogHandler.addEventListener('click', onClickPreventDefault);
-    }
 
-  };
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
 
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
+      popup.style.top = (popup.offsetTop - shift.y) + 'px';
+      popup.style.left = (popup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      if (dragged) {
+        var onClickPreventDefault = function (evtDrgd) {
+          evtDrgd.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+    popupClose.addEventListener('click', defaultPopupCoordinates);
+  });
+};
+
+popupOpen.addEventListener('click', onDialogHandlerClick);
