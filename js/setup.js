85f8152047f2c58d8a8dd2c11a7a376bd7dfca5e
msdubova <femme777@gmail.com>
2019-06-27 20:01:47 +0200

    Добавила глобальную область util.js

--- js/setup.js
+++ js/setup.js
@@ -1,254 +1,226 @@
 'use strict';
 (function () {
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
-  var setupPopup = document.querySelector('.setup');
-  var openSetup = document.querySelector('.setup-open');
-  var form = setupPopup.querySelector('.setup-wizard-form');
-  var userNameInput = setupPopup.querySelector('.setup-user-name');
-  var similarWizardsList = document.querySelector('.setup-similar-list');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  var addRandomWizards = function () {
+    var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+    var fragment = document.createDocumentFragment();
 
-  var WIZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-  var fragment = document.createDocumentFragment();
-
-  var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
-  var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
-  var wizardFireball = document.querySelector('.setup-fireball-wrap');
-
-  var coatInput = form.querySelector('input[name="coat-color"]');
-  var eyesInput = form.querySelector('input[name="eyes-color"]');
-  var fireballInput = form.querySelector('input[name="fireball-color"]');
-  var isFocus = false;
-
-  /**
-       * Функция создает случайное число в диапазоне ячеек массива опрокинутого в эту функцию параметром с учетом его длины
-       * @param {number} arr  - принимает на вход длину массива с данными для того, чтоб вычислить длину массива и ограничить диапазон случайной выборки
-       * @return {number} возвращает случайную ячейку массива из существующих в массиве
-       */
-  var getRandomInRange = function (arr) {
-    var min = 0;
-    return Math.floor(Math.random() * ((arr.length - 1) - min + 1)) + min;
-  };
-
-  /**
-   * Функция создает имя персонажа
-   * @param {arr} names массив имен
-   * @param {arr} surnames массив фамилий
-   * @return {string} имя фамилия
-   */
-  var generateFullName = function (names, surnames) {
-    return (names[getRandomInRange(names)] + ' ' + surnames[getRandomInRange(surnames)]);
-  };
-
-  /**
-     * Функция создает случайные стили
-     * @param {arr} styles массив со стилями
-     * @return {string} возвращает случайный стиль
+    /**
+     * Функция создает имя персонажа
+     * @param {arr} names массив имен
+     * @param {arr} surnames массив фамилий
+     * @return {string} имя фамилия
      */
-  var generateStyle = function (styles) {
-    return (styles[getRandomInRange(styles)]);
-  };
+    var generateFullName = function (names, surnames) {
+      return (names[window.util.getRandomInRange(names)] + ' ' + surnames[window.util.getRandomInRange(surnames)]);
+    };
 
-  /**
-   * Функция добавляет персонажей в разметку
-   */
-  var createWizards = function () {
-    for (var i = 0; i < 4; i++) {
-      var wizardCloned = similarWizardTemplate.cloneNode(true);
+    /**
+     * Функция добавляет персонажей в разметку
+     */
+    var createWizards = function () {
+      for (var i = 0; i < 4; i++) {
+        var wizardCloned = similarWizardTemplate.cloneNode(true);
 
-      wizardCloned.querySelector('.setup-similar-label').textContent = generateFullName(WIZARD_NAMES, WIZARD_SURNAMES);
-      wizardCloned.querySelector('.wizard-coat').style.fill = generateStyle(COAT_COLORS);
-      wizardCloned.querySelector('.wizard-eyes').style.fill = generateStyle(EYES_COLORS);
+        wizardCloned.querySelector('.setup-similar-label').textContent = generateFullName(window.util.WIZARD_NAMES, window.util.WIZARD_SURNAMES);
+        wizardCloned.querySelector('.wizard-coat').style.fill = window.util.generateStyle(window.util.COAT_COLORS);
+        wizardCloned.querySelector('.wizard-eyes').style.fill = window.util.generateStyle(window.util.EYES_COLORS);
 
-      fragment.appendChild(wizardCloned);
-      similarWizardsList.appendChild(fragment);
-    }
-  };
+        fragment.appendChild(wizardCloned);
+        window.util.similarWizardsList.appendChild(fragment);
+      }
+    };
 
-  /**
-   * Функция удаляет класс hidden у элемента, тем самым показывая его на странице, а также добавляет обработчик событий на документ и на кнопку формы
-   */
-  var onPopupOpenClick = function () {
-    var submitButton = setupPopup.querySelector('.setup-submit');
-    var closeSetup = setupPopup.querySelector('.setup-close');
-    openSetup.removeEventListener('click', onPopupOpenClick);
-
-    setupPopup.classList.remove('hidden');
     createWizards();
-    document.querySelector('.setup-similar').classList.remove('hidden');
-    userNameInput.addEventListener('focus', onUserNameInputFocus);
-    userNameInput.addEventListener('blur', onUserNameInputBlur);
-    document.addEventListener('keydown', onPopupEscPress);
-    submitButton.addEventListener('click', onButtonClick);
-    submitButton.addEventListener('keydown', onButtonPush);
-    wizardCoat.addEventListener('click', onWizardCoatClick);
-    wizardEyes.addEventListener('click', onWizardEyesClick);
-    wizardFireball.addEventListener('click', onWizardFireballClick);
-
-    closeSetup.addEventListener('click', onCloseSetupClick, {once: true});
-
-    closeSetup.addEventListener('keydown', onCloseSetupPush, {once: true});
   };
 
-  /**
-   * Функция- обработчик, открывает попап при нажатии энтер
-   * @param {object} evt объект события, который передаётся первым аргументом в обработчик
-   */
-  var onPopupOpenPush = function (evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
-      onPopupOpenClick();
-    }
-  };
 
-  /**
-   * Функция добавляет элементу класс hidden, тем самым скрывая его, а также удаляет обработчик событий с документа и с кнопки формы
-   */
-  var closePopup = function () {
-    var submitButton = setupPopup.querySelector('.setup-submit');
-    openSetup.addEventListener('click', onPopupOpenClick);
-    similarWizardsList.innerHTML = '';
+  (function () {
+    var openSetup = document.querySelector('.setup-open');
+    var isFocus = false;
 
-    setupPopup.classList.add('hidden');
-    document.querySelector('.setup-similar').classList.add('hidden');
-    document.removeEventListener('keydown', onPopupEscPress);
-    submitButton.removeEventListener('click', onButtonClick);
-    submitButton.removeEventListener('keydown', onButtonPush);
-    userNameInput.removeEventListener('focus', onUserNameInputFocus);
-    userNameInput.removeEventListener('blur', onUserNameInputBlur);
-    wizardCoat.removeEventListener('click', onWizardCoatClick);
-    wizardEyes.removeEventListener('click', onWizardEyesClick);
-    wizardFireball.removeEventListener('click', onWizardFireballClick);
-  };
+    var coatInput = window.util.form.querySelector('input[name="coat-color"]');
+    var eyesInput = window.util.form.querySelector('input[name="eyes-color"]');
+    var fireballInput = window.util.form.querySelector('input[name="fireball-color"]');
 
-  /**
-   * Функция-обработчик  - закрывает попап
-   */
-  var onCloseSetupClick = function () {
-    closePopup();
-  };
+    /**
+     * Функция удаляет класс hidden у элемента, тем самым показывая его на странице, а также добавляет обработчик событий на документ и на кнопку формы
+     */
+    var onPopupOpenClick = function () {
+      var submitButton = window.util.setupPopup.querySelector('.setup-submit');
+      var closeSetup = window.util.setupPopup.querySelector('.setup-close');
+      openSetup.removeEventListener('click', onPopupOpenClick);
+      window.util.setupPopup.classList.remove('hidden');
+      addRandomWizards();
+      document.querySelector('.setup-similar').classList.remove('hidden');
+      window.util.userNameInput.addEventListener('focus', onUserNameInputFocus);
+      window.util.userNameInput.addEventListener('blur', onUserNameInputBlur);
+      document.addEventListener('keydown', onPopupEscPress);
+      submitButton.addEventListener('click', onButtonClick);
+      submitButton.addEventListener('keydown', onButtonPush);
+      window.util.wizardCoat.addEventListener('click', onWizardCoatClick);
+      window.util.wizardEyes.addEventListener('click', onWizardEyesClick);
+      window.util.wizardFireball.addEventListener('click', onWizardFireballClick);
 
-  /**
-   * Функция- обрабочик - закрывает попап по нажатию эскейпа
-   * @param {object} evt объект события, который передаётся первым аргументом в обработчик
-   */
-  var onCloseSetupPush = function (evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
-      closePopup();
-    }
-  };
+      closeSetup.addEventListener('click', onCloseSetupClick, {once: true});
 
-  /**
-   * Функция - обработчик, слушает элемент на предмет клика клавиши 27 (Escape)и возвращает выполнение функции closePopup
-   * @param {object} evt объект события, который передаётся первым аргументом в обработчик
-   */
-  var onPopupEscPress = function (evt) {
-    if ((evt.keyCode === ESC_KEYCODE) && (isFocus === false)) {
-      onCloseSetupClick();
-    }
-  };
+      closeSetup.addEventListener('keydown', onCloseSetupPush, {once: true});
+    };
 
-  /**
-   * Функция-обработчик - создает флаг для функции закрытия попапа - при isFocus = true закрытие не произойдет
-   */
-  var onUserNameInputFocus = function () {
-    isFocus = true;
-  };
-
-  /**
-   * Функция-обработчик - создает флаг для функции закрытия попапа - при isFocus = false закрытие произойдет
-   */
-  var onUserNameInputBlur = function () {
-    isFocus = false;
-  };
-
-  /**
-   * Функция проверяет валидность формы при клике
-   * @param {object} evt «объект события», который передаётся первым аргументом в обработчик
-   */
-  var onButtonClick = function () {
-    userNameInput.addEventListener('invalid', function (evt) {
-      getCustomValidityMessage(evt);
-    });
-  };
-
-  /**
-   * Функция проверяет валидность формы при нажатии на клавишу
-   * @param {object} evt объект события, который передаётся первым аргументом в обработчик
-   */
-  var onButtonPush = function (evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
-      userNameInput.addEventListener('invalid', function () {
+    /**
+     * Функция проверяет валидность формы при клике
+     * @param {object} evt «объект события», который передаётся первым аргументом в обработчик
+     */
+    var onButtonClick = function () {
+      window.util.userNameInput.addEventListener('invalid', function (evt) {
         getCustomValidityMessage(evt);
       });
-    }
-  };
+    };
 
-  /**
-   * Функция кастомизирует значения сообщений о невалидности
-   */
-  var getCustomValidityMessage = function () {
-    if (userNameInput.validity.tooShort) {
-      userNameInput.setCustomValidity('Минимальное значение символов - 2. Добавьте символы');
-    } else if (userNameInput.validity.tooLong) {
-      userNameInput.setCustomValidity('Минимальное значение символов - 25. Удалите лишние символы');
-    } else if (userNameInput.validity.valueMissing) {
-      userNameInput.setCustomValidity('Это поле обязательное для заполнения');
-    } else {
-      userNameInput.setCustomValidity('');
-    }
-  };
+    /**
+     * Функция проверяет валидность формы при нажатии на клавишу
+     * @param {object} evt объект события, который передаётся первым аргументом в обработчик
+     */
+    var onButtonPush = function (evt) {
+      if (evt.keyCode === window.util.ENTER_KEYCODE) {
+        window.util.userNameInput.addEventListener('invalid', function () {
+          getCustomValidityMessage(evt);
+        });
+      }
+    };
 
-  /**
-   * Функция присваивает случайный стиль fill элементу и передает этот цвет в форму
-   * @param {object} item объект который должен быть прослушан на предмет клика
-   * @param {string[]} array массив стилей для этого обьекта
-   * @param {object} input поле формы, в которое будет записаны данные стиля
-   */
-  var setRandomColor = function (item, array, input) {
-    input.value = generateStyle(array);
-    item.style.fill = input.value;
-  };
+      /**
+     * Функция присваивает случайный стиль fill элементу и передает этот цвет в форму
+     * @param {object} item объект который должен быть прослушан на предмет клика
+     * @param {string[]} array массив стилей для этого обьекта
+     * @param {object} input поле формы, в которое будет записаны данные стиля
+     */
+    var setRandomColor = function (item, array, input) {
+      input.value = window.util.generateStyle(array);
+      item.style.fill = input.value;
+    };
 
-  /**
-   * Функция присваивает случайный стиль background элементу
-   * @param {object} item
-   * @param {string[]} array
-   * @param {object} input
-   */
-  var setRandomBackground = function (item, array, input) {
-    input.value = generateStyle(array);
-    item.style.background = input.value;
-  };
+    /**
+     * Функция присваивает случайный стиль background элементу
+     * @param {object} item
+     * @param {string[]} array
+     * @param {object} input
+     */
+    var setRandomBackground = function (item, array, input) {
+      input.value = window.util.generateStyle(array);
+      item.style.background = input.value;
+    };
 
-  /**
-   * Функция-обработчик, срабатывает при клике на пальто мага в открытом попапе и запустит установку случайного цвета пальто
-   */
-  var onWizardCoatClick = function () {
-    setRandomColor(wizardCoat, COAT_COLORS, coatInput);
-  };
+    /**
+     * Функция-обработчик, срабатывает при клике на пальто мага в открытом попапе и запустит установку случайного цвета пальто
+     */
+    var onWizardCoatClick = function () {
+      setRandomColor(window.util.wizardCoat, window.util.COAT_COLORS, coatInput);
+    };
 
-  /**
-   * Функция-обработчик, срабатывает при клике на глаза мага в открытом попапе и запутит установку случайного цвета глаз мага
-   */
-  var onWizardEyesClick = function () {
-    setRandomColor(wizardEyes, EYES_COLORS, eyesInput);
-  };
+    /**
+     * Функция-обработчик, срабатывает при клике на глаза мага в открытом попапе и запутит установку случайного цвета глаз мага
+     */
+    var onWizardEyesClick = function () {
+      setRandomColor(window.util.wizardEyes, window.util.EYES_COLORS, eyesInput);
+    };
 
-  /**
-   * Функция-обработчик, срабатывает при клике на фаербол мага в открытом попапе и запутит установку случайного цвета фаербола мага
-   */
-  var onWizardFireballClick = function () {
-    setRandomBackground(wizardFireball, FIREBALL_COLORS, fireballInput);
-  };
+    /**
+     * Функция-обработчик, срабатывает при клике на фаербол мага в открытом попапе и запутит установку случайного цвета фаербола мага
+     */
+    var onWizardFireballClick = function () {
+      setRandomBackground(window.util.wizardFireball, window.util.FIREBALL_COLORS, fireballInput);
+    };
 
-  openSetup.addEventListener('click', onPopupOpenClick);
+    /**
+     * Функция- обработчик, открывает попап при нажатии энтер
+     * @param {object} evt объект события, который передаётся первым аргументом в обработчик
+     */
+    var onPopupOpenPush = function (evt) {
+      if (evt.keyCode === window.util.ENTER_KEYCODE) {
+        onPopupOpenClick();
+      }
+    };
 
-  openSetup.addEventListener('keydown', onPopupOpenPush);
+    /**
+     * Функция добавляет элементу класс hidden, тем самым скрывая его, а также удаляет обработчик событий с документа и с кнопки формы
+     */
+    var closePopup = function () {
+      var submitButton = window.util.setupPopup.querySelector('.setup-submit');
+      openSetup.addEventListener('click', onPopupOpenClick);
+      window.util.similarWizardsList.innerHTML = '';
+
+      window.util.setupPopup.classList.add('hidden');
+      document.querySelector('.setup-similar').classList.add('hidden');
+      document.removeEventListener('keydown', onPopupEscPress);
+      submitButton.removeEventListener('click', onButtonClick);
+      submitButton.removeEventListener('keydown', onButtonPush);
+      window.util.userNameInput.removeEventListener('focus', onUserNameInputFocus);
+      window.util.userNameInput.removeEventListener('blur', onUserNameInputBlur);
+      window.util.wizardCoat.removeEventListener('click', onWizardCoatClick);
+      window.util.wizardEyes.removeEventListener('click', onWizardEyesClick);
+      window.util.wizardFireball.removeEventListener('click', onWizardFireballClick);
+    };
+
+    /**
+     * Функция-обработчик - создает флаг для функции закрытия попапа - при isFocus = true закрытие не произойдет
+     */
+    var onUserNameInputFocus = function () {
+      isFocus = true;
+    };
+
+    /**
+     * Функция-обработчик - создает флаг для функции закрытия попапа - при isFocus = false закрытие произойдет
+     */
+    var onUserNameInputBlur = function () {
+      isFocus = false;
+    };
+
+    /**
+     * Функция-обработчик  - закрывает попап
+     */
+    var onCloseSetupClick = function () {
+      closePopup();
+    };
+
+    /**
+     * Функция- обрабочик - закрывает попап по нажатию эскейпа
+     * @param {object} evt объект события, который передаётся первым аргументом в обработчик
+     */
+    var onCloseSetupPush = function (evt) {
+      if (evt.keyCode === window.util.ENTER_KEYCODE) {
+        closePopup();
+      }
+    };
+
+    /**
+     * Функция - обработчик, слушает элемент на предмет клика клавиши 27 (Escape)и возвращает выполнение функции closePopup
+     * @param {object} evt объект события, который передаётся первым аргументом в обработчик
+     */
+    var onPopupEscPress = function (evt) {
+      if ((evt.keyCode === window.util.ESC_KEYCODE) && (isFocus === false)) {
+        onCloseSetupClick();
+      }
+    };
+
+      /**
+     * Функция кастомизирует значения сообщений о невалидности
+     */
+    var getCustomValidityMessage = function () {
+      if (window.util.userNameInput.validity.tooShort) {
+        window.util.userNameInput.setCustomValidity('Минимальное значение символов - 2. Добавьте символы');
+      } else if (window.util.userNameInput.validity.tooLong) {
+        window.util.userNameInput.setCustomValidity('Минимальное значение символов - 25. Удалите лишние символы');
+      } else if (window.util.userNameInput.validity.valueMissing) {
+        window.util.userNameInput.setCustomValidity('Это поле обязательное для заполнения');
+      } else {
+        window.util.userNameInput.setCustomValidity('');
+      }
+    };
+    openSetup.addEventListener('click', onPopupOpenClick);
+
+    openSetup.addEventListener('keydown', onPopupOpenPush);
+
+  })();
 
 })();
 
