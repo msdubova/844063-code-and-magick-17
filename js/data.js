e10db2a3750d299c8b7355ef01fe034248ef0b6d
msdubova <femme777@gmail.com>
2019-07-04 14:19:46 +0200

    Добавила сообщение

--- js/data.js
+++ js/data.js
@@ -33,7 +33,31 @@
     var node = document.createElement('div');
     node.style = 'z-index: 100; width: 500px; height: auto; padding: 20px; top: 50%; left: 50%; transform: translate(-50%, -50%); position: absolute;  font-size: inherit; text-align: center;   background-color: tomato;';
 
-    node.textContent = errorNotification;
+    switch (errorNotification) {
+      case 300:
+        node.textContent = 'Multiple Choice';
+        break;
+      case 301:
+        node.textContent = 'Moved Permanently';
+        break;
+      case 307:
+        node.textContent = 'Temporary Redirect';
+        break;
+      case 400:
+        node.textContent = 'Bad Request';
+        break;
+      case 401:
+        node.textContent = 'Access denied';
+        break;
+      case 404:
+        node.textContent = 'Not found';
+        break;
+      case 500:
+        node.textContent = 'Internal Server Error';
+        break;
+      default:
+        node.textContent = 'Request status: ' + status;
+    }
     document.body.insertAdjacentElement('afterbegin', node);
   };
 
